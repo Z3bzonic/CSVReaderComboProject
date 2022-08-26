cfb8b42198056cfcb143829eedf9d680f1895d53
Z3bzonic <107925003+Z3bzonic@users.noreply.github.com>
2022-08-26 12:14:40 +0200

    Add files via upload

---
+++ CSVReader/Program.cs
@@ -0,0 +1,8 @@
+﻿var strm = new StreamReader((Environment.CurrentDirectory).ToString());
+//(@"../../../../telewerkers.csv");
+var strmline = new List<string>();
+
+while (!strm.EndOfStream)
+{
+    strm.
+}
